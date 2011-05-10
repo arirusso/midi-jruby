c1e45e164131eac900ed0847acc9d19e1f6bc4d0
Ari Russo <ari.russo@gmail.com>
2011-05-09 22:32:00 -0400

    version

--- lib/midi-jruby.rb
+++ lib/midi-jruby.rb
@@ -1,14 +1,15 @@
 require 'java'
+require 'forwardable' 
 
 #
 # Set of modules and classes for interacting with javax.sound.midi
 #
 module MIDIJRuby
   
-    VERSION = "0.0.4"
+    VERSION = "0.0.5"
        
 end
- 
+
 require 'midi-jruby/device'
 require 'midi-jruby/input'
 require 'midi-jruby/output'
